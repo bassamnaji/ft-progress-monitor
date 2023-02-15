b88009d5ce44c49350ec52cf9efabdbe17287411
Hussain Awadh <mister.tooh@gmail.com>
2023-02-15 18:44:55 +0400

    PROTRACK-12

--- app/api/src/users/users.module.ts
+++ app/api/src/users/users.module.ts
@@ -1,9 +1,13 @@
-import { Module } from '@nestjs/common';
-import { UsersService } from './users.service';
-import { UsersController } from './users.controller';
+import { Module } from '@nestjs/common'
+import { User } from './entities/user.entity'
+import { UsersService } from './users.service'
+import { UsersController } from './users.controller'
+import { TypeOrmModule } from '@nestjs/typeorm'
 
 @Module({
-  controllers: [UsersController],
-  providers: [UsersService]
+    imports: [TypeOrmModule.forFeature([User])],
+    controllers: [UsersController],
+    providers: [UsersService],
+    exports: [TypeOrmModule]
 })
 export class UsersModule {}
--- app/api/src/users/users.service.ts
+++ app/api/src/users/users.service.ts
@@ -1,26 +1,59 @@
-import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import { HttpStatus, Injectable, NotFoundException } from '@nestjs/common'
+import { InjectRepository } from '@nestjs/typeorm'
+import { Repository } from 'typeorm'
+import { CreateUserDto } from './dto/create-user.dto'
+import { UpdateUserDto } from './dto/update-user.dto'
+import { User } from './entities/user.entity'
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
-  }
+    constructor(
+        @InjectRepository(User) private userRepository: Repository<User>
+    ) {}
 
-  findAll() {
-    return `This action returns all users`;
-  }
+    async findAll() {
+        return await this.userRepository.find()
+    }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
-  }
+    async findOne(id: number) {
+        const user = await this.userRepository.findOneBy({ id })
+        if (!user) {
+            throw new NotFoundException()
+        }
+        return user
+    }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
+    async findOrCreate(id: number, userDto: CreateUserDto) {
+        let user = await this.userRepository.findOneBy({ id })
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
-  }
+        const httpStatus = user ? HttpStatus.CREATED : HttpStatus.OK
+
+        if (!user) {
+            user = await this.userRepository.save(userDto)
+        }
+
+        return { httpStatus, user }
+    }
+
+    async update(id: number, updateUserDto: UpdateUserDto) {
+        const user = await this.userRepository.findOneBy({ id })
+
+        if (!user) {
+            throw new NotFoundException()
+        }
+
+        return
+    }
+
+    async remove(id: number) {
+        const user = await this.userRepository.findOneBy({ id })
+
+        if (!user) {
+            throw new NotFoundException()
+        } else {
+            await this.userRepository.delete({ id })
+        }
+
+        return
+    }
 }
